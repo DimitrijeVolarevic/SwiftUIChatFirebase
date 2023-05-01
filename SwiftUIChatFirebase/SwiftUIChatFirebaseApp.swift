5ab92b5932730aa7a7116491657d60132182c4df
DimitrijeVolarevic <dimitrijevolarevic9@gmail.com>
2023-05-01 16:44:04 +0200

    Initial Commit

---
+++ SwiftUIChatFirebase/SwiftUIChatFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatFirebaseApp.swift
+//  SwiftUIChatFirebase
+//
+//  Created by Dimitrije Volarevic on 1.5.23..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
